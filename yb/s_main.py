358d8b2520781c3d19e5fb62095546dc8cc9625c
say <say@gmail.com>
2016-10-25 18:11:44 +0800

    add

--- yb/s_main.py
+++ yb/s_main.py
@@ -3,6 +3,26 @@ Created on 2016年10月25日
 
 @author: sun
 '''
+
+from yb import url_manager
+from yb import html_downloader
+from yb import html_parser
+from yb import html_outputer
+
+class SpiderMain(object):
+    def _init_(self):
+        self.urls=url_manager.UrlManager();
+        self.downloader=html_downloader.HtmlDownloader();
+        self.parser=html_parser.HtmlParser();
+        self.outputer=html_outputer.HtmlOutputer();
+    
+    def craw(self, root_url):
+        pass
+    
+    
+
+
+
 if _name_=="_main_":
     root_url="https://www.yuanbao.com/news/?corpid=0"
     obj_spider=SpiderMain()
